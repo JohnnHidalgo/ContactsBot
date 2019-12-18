39569c1627da60d34083586a377dd46010694509
JohnnHidalgo <johnna.7h@gmail.com>
2019-12-18 11:21:55 -0400

    Initial commit

---
+++ src/main/java/com/example/AgendaSoftware/AgendaSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.example.AgendaSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AgendaSoftware/AgendaSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AgendaSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
