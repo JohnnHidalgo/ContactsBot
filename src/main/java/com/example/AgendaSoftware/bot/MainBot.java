419b4e661b92fb8946ea9e51def1eacf5410bbe3
JohnnHidalgo <johnna.7h@gmail.com>
2019-12-18 19:25:30 -0400

    Update MainBot for Multiuser

--- src/main/java/com/example/AgendaSoftware/bot/MainBot.java
+++ src/main/java/com/example/AgendaSoftware/bot/MainBot.java
@@ -1,32 +1,35 @@
 package com.example.AgendaSoftware.bot;
 
-import com.example.AgendaSoftware.bl.PhoneBl;
-import com.example.AgendaSoftware.domain.Phone;
+import com.example.AgendaSoftware.bl.BotBl;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import java.util.List;
 
 public class MainBot extends TelegramLongPollingBot {
 
-    PhoneBl phoneBl;
+    BotBl botBl;
 
-    public MainBot(PhoneBl phoneBl){
-        this.phoneBl = phoneBl;
+    public MainBot(BotBl botBl){
+        this.botBl = botBl;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
         System.out.println(update);
+        update.getMessage().getFrom().getId();
         if (update.hasMessage() && update.getMessage().hasText()) {
-            Phone phone = phoneBl.findPhoneById(1);
-            SendMessage message = new SendMessage()
-                    .setChatId(update.getMessage().getChatId())
-                    .setText("Phone from: " + phone.getNumberPhone());
-            try {
-                this.execute(message);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+            List<String> messages = botBl.processUpdate(update);
+            for(String messageText: messages) {
+                SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                        .setChatId(update.getMessage().getChatId())
+                        .setText(messageText);
+                try {
+                    this.execute(message);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
