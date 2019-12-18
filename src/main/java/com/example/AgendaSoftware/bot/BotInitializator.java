929d4000eb0ad229a749dcd7a0e8be5a4f9a643a
JohnnHidalgo <johnna.7h@gmail.com>
2019-12-18 19:24:24 -0400

    Add multiUser chabges for Bot

--- src/main/java/com/example/AgendaSoftware/bot/BotInitializator.java
+++ src/main/java/com/example/AgendaSoftware/bot/BotInitializator.java
@@ -1,4 +1,34 @@
 package com.example.AgendaSoftware.bot;
 
+import com.example.AgendaSoftware.bl.BotBl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import javax.annotation.PostConstruct;
+
+@Component
 public class BotInitializator {
+
+    BotBl botBl;
+
+    @Autowired
+    public BotInitializator(BotBl botBl) {
+        this.botBl = botBl;
+    }
+
+    public BotInitializator() {
+    }
+
+    @PostConstruct
+    public void init() {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new MainBot(botBl));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 }
